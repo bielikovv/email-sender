8611987268bf3781e686d8fb2b89f764fa9b269c
bielikovv <ivanbelikov890@gmail.com>
2022-03-28 16:29:47 +0300

    form changing 2.0

--- email_sender/sender/forms.py
+++ email_sender/sender/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class SendMailForm(forms.ModelForm):
     subject = forms.CharField(label = 'Theme', widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(label = 'Content', widget=forms.Textarea(attrs={'class': 'form-control'}))
+
