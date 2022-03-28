beac8892cf30da50a8d078532df50333659e60bc
bielikovv <ivanbelikov890@gmail.com>
2022-03-28 16:11:09 +0300

    host settings

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'fakelazzex@gmail.com'
+EMAIL_HOST_PASSWORD = 'vitrum15'
+
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
