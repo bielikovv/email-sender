8920f93c1147df17ea221a035321efabfc7d4b81
bielikovv <ivanbelikov890@gmail.com>
2022-03-28 18:41:57 +0300

    adding messages but it needs to fix

--- email_sender/sender/views.py
+++ email_sender/sender/views.py
@@ -2,14 +2,19 @@ from .models import *
 from .forms import *
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def mail(request):
     if request.method == 'POST':
         form = SendMailForm(request.POST)
         if form.is_valid():
-            send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'fakelazzex@gmail.com', [form.cleaned_data['name']], )
-            return redirect('main_page')
+            mail = send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'fakelazzex@gmail.com', [form.cleaned_data['name']],)
+            if mail:
+                messages.success(request, 'Message sent!')
+                return redirect('main_page')
+            else:
+                messages.error(request, 'Fail!')
     else:
         form = SendMailForm()
     return render(request, 'sender/main_page.html', {'form':form})
