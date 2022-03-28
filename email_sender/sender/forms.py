bc396da8062f840ccbec14fbece7da4bdaa2c18d
bielikovv <ivanbelikov890@gmail.com>
2022-03-28 16:40:41 +0300

    changes in models and forms

--- email_sender/sender/forms.py
+++ email_sender/sender/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class SendMailForm(forms.ModelForm):
     subject = forms.CharField(label = 'Theme', widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(label = 'Content', widget=forms.Textarea(attrs={'class': 'form-control'}))
-
+    name = forms.EmailField(label = 'E-mail', widget=forms.Textarea(attrs={'class': 'form-control'}))
--- email_sender/sender/models.py
+++ email_sender/sender/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class SendMail(models.Model):
     theme = models.CharField(max_length=200, verbose_name='theme')
     content = models.TextField(verbose_name='content')
+    name = models.EmailField(verbose_name='mail', null=True)
