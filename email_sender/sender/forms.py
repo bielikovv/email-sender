229915a855f353af7fb141e96334348ef2bed156
bielikovv <ivanbelikov890@gmail.com>
2022-03-28 17:22:20 +0300

    sending mail - complete

--- email_sender/sender/forms.py
+++ email_sender/sender/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from .models import *
 
 
 class SendMailForm(forms.ModelForm):
     subject = forms.CharField(label = 'Theme', widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(label = 'Content', widget=forms.Textarea(attrs={'class': 'form-control'}))
     name = forms.EmailField(label = 'E-mail', widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = SendMail
+        fields = ['name', 'subject', 'content']
--- email_sender/sender/views.py
+++ email_sender/sender/views.py
@@ -1,15 +1,15 @@
 from .models import *
 from .forms import *
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def mail(request):
     if request.method == 'POST':
         form = SendMailForm(request.POST)
         if form.is_valid():
-            send_mail(form.cleaned_data(['subject'], form.cleaned_data(['content']), 'fakelazzex@gmail.com', ['name'], fail_sslently = False))
-            return
-        else:
-            form = SendMailForm()
-    return render(request, '', {'form':form})
+            send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'fakelazzex@gmail.com', ['ivanbelikov890@gmail.com'], )
+            return redirect('main_page')
+    else:
+        form = SendMailForm()
+    return render(request, 'sender/main_page.html', {'form':form})
